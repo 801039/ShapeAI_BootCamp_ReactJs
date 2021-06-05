55868e271d20eadabe3b97a2e6e8c823f788a2e5
Tharindu Sanjitha (Alpha) <65927967+801039@users.noreply.github.com>
2021-06-05 12:28:29 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div class="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing boot camp taken up by Shaurya Singha. We covered
+        everything from scratch including Javascript and React.js, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
